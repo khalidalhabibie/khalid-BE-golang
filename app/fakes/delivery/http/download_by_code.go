71ede82aba12e8f4b344dd15aadba37c9bcc0e78
Khalid <khalidalhabibie07@gmail.com>
2022-03-25 15:52:18 +0700

    :bug: checking fakes by code

--- app/fakes/delivery/http/download_by_code.go
+++ app/fakes/delivery/http/download_by_code.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"fmt"
+	fakesUsecase "gokes/app/fakes/usecase"
+	"gokes/pkg/utils"
 
 	fiber "github.com/gofiber/fiber/v2"
 )
@@ -9,6 +11,11 @@ import (
 func Download(c *fiber.Ctx) error {
 
 	code := c.Params("code")
+
+	_, err := fakesUsecase.FindByCode(code)
+	if err != nil {
+		utils.ReturnFormat(c, fiber.StatusNotFound, true, "data not found", nil)
+	}
 
 	return c.SendFile(fmt.Sprintf("data/%v.pdf", code), true)
 
